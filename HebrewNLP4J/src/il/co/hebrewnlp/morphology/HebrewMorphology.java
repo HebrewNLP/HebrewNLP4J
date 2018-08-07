c9416fc36eb2dfc580b48f8d6b93627b893abf02
Itay <itay2805@protonmail.com>
2018-08-07 10:14:05 +0300

    Added normalization type

--- HebrewNLP4J/src/il/co/hebrewnlp/morphology/HebrewMorphology.java
+++ HebrewNLP4J/src/il/co/hebrewnlp/morphology/HebrewMorphology.java
@@ -1,5 +1,6 @@
 package il.co.hebrewnlp.morphology;
 
+import java.io.Serializable;
 import java.util.Collection;
 
 import com.google.gson.Gson;
@@ -14,7 +15,13 @@ public class HebrewMorphology {
     public static final String MORPH_ANALYZE_ENDPOINT = "/service/morphology/analyze";
     private static final Gson GSON = new GsonBuilder().create();
 	
-    private static class MorphRequest {
+    public enum NormalizationType implements Serializable, Cloneable {
+    	SEARCH,
+    	INDEX,
+    }
+    
+    
+    private static class MorphologyRequest {
     	
     	@SuppressWarnings("unused")
 		public String token;
@@ -32,16 +39,44 @@ public class HebrewMorphology {
 		public String[] sentences;
     }
     
+    private static class NormalizationRequest {
+    	
+    	@SuppressWarnings("unused")
+		public String token;
+    	
+    	@SuppressWarnings("unused")
+		public NormalizationType type;
+
+    	@SuppressWarnings("unused")
+		public String[] words;
+    	
+    	@SuppressWarnings("unused")
+		public String text;
+    	
+    	@SuppressWarnings("unused")
+		public String sentence;
+    	
+    	@SuppressWarnings("unused")
+		public String[] sentences;
+    }
+    
     private static class MorphErrorResponse {
     	public String error;
     }
+    
+    /*----------------------------------------------------------------------------*/
 
     public static String[][] normalizeText(String text) throws Exception {
+    	return normalizeText(text, NormalizationType.SEARCH);
+    }
+    
+    public static String[][] normalizeText(String text, NormalizationType type) throws Exception {
     	if(HebrewNLP.getPassword() == null) {
     		throw new IllegalStateException("Please set HebrewNLP.setPassword() method with your password before using this method. To get a password register at https://hebrew-nlp.co.il/registration.");
     	}
-    	MorphRequest request = new MorphRequest();
+    	NormalizationRequest request = new NormalizationRequest();
     	request.token = HebrewNLP.getPassword();
+    	request.type = type;
     	request.text = text;
     	String requestJson = GSON.toJson(request);
     	String responseJson = Util.postJSONData(MORPH_NORMALIZE_ENDPOINT, requestJson);
@@ -52,15 +87,24 @@ public class HebrewMorphology {
     }
     
     public static String[][] normalizeSentences(Collection<String> sentences) throws Exception {
-    	return normalizeSentences(sentences.toArray(new String[sentences.size()]));
+    	return normalizeSentences(sentences, NormalizationType.SEARCH);
+    }
+    
+    public static String[][] normalizeSentences(Collection<String> sentences, NormalizationType type) throws Exception {
+    	return normalizeSentences(sentences.toArray(new String[sentences.size()]), type);
     }
 
     public static String[][] normalizeSentences(String[] sentences) throws Exception {
+    	return normalizeSentences(sentences, NormalizationType.SEARCH);
+    }
+    
+    public static String[][] normalizeSentences(String[] sentences, NormalizationType type) throws Exception {
     	if(HebrewNLP.getPassword() == null) {
     		throw new IllegalStateException("Please set HebrewNLP.setPassword() method with your password before using this method. To get a password register at https://hebrew-nlp.co.il/registration.");
     	}
-    	MorphRequest request = new MorphRequest();
+    	NormalizationRequest request = new NormalizationRequest();
     	request.token = HebrewNLP.getPassword();
+    	request.type = type;
     	request.sentences = sentences;
     	String requestJson = GSON.toJson(request);
     	String responseJson = Util.postJSONData(MORPH_NORMALIZE_ENDPOINT, requestJson);
@@ -71,11 +115,16 @@ public class HebrewMorphology {
     }
     
     public static String[] normalizeSentence(String sentence) throws Exception {
+    	return normalizeSentence(sentence, NormalizationType.SEARCH);
+    }
+    
+    public static String[] normalizeSentence(String sentence, NormalizationType type) throws Exception {
     	if(HebrewNLP.getPassword() == null) {
     		throw new IllegalStateException("Please set HebrewNLP.setPassword() method with your password before using this method. To get a password register at https://hebrew-nlp.co.il/registration.");
     	}
-    	MorphRequest request = new MorphRequest();
+    	NormalizationRequest request = new NormalizationRequest();
     	request.token = HebrewNLP.getPassword();
+    	request.type = type;
     	request.sentence = sentence;
     	String requestJson = GSON.toJson(request);
     	String responseJson = Util.postJSONData(MORPH_NORMALIZE_ENDPOINT, requestJson);
@@ -86,15 +135,24 @@ public class HebrewMorphology {
     }
     
     public static String[] normalizeWords(Collection<String> words) throws Exception {
-    	return normalizeWords(words.toArray(new String[words.size()]));
+    	return normalizeWords(words, NormalizationType.SEARCH);
     }
     
+    public static String[] normalizeWords(Collection<String> words, NormalizationType type) throws Exception {
+    	return normalizeWords(words.toArray(new String[words.size()]), type);
+    }
+
     public static String[] normalizeWords(String[] words) throws Exception {
+    	return normalizeWords(words, NormalizationType.SEARCH);
+    }
+    
+    public static String[] normalizeWords(String[] words, NormalizationType type) throws Exception {
     	if(HebrewNLP.getPassword() == null) {
     		throw new IllegalStateException("Please set HebrewNLP.setPassword() method with your password before using this method. To get a password register at https://hebrew-nlp.co.il/registration.");
     	}
-    	MorphRequest request = new MorphRequest();
+    	NormalizationRequest request = new NormalizationRequest();
     	request.token = HebrewNLP.getPassword();
+    	request.type = type;
     	request.words = words;
     	String requestJson = GSON.toJson(request);
     	String responseJson = Util.postJSONData(MORPH_NORMALIZE_ENDPOINT, requestJson);
@@ -105,14 +163,20 @@ public class HebrewMorphology {
     }
 
     public static String normalizeWord(String word) throws Exception {
-    	return normalizeWords(new String[] { word })[0];
+    	return normalizeWord(word, NormalizationType.SEARCH);
     }
+    
+    public static String normalizeWord(String word, NormalizationType type) throws Exception {
+    	return normalizeWords(new String[] { word }, type)[0];
+    }
+    
+    /*----------------------------------------------------------------------------*/
     
     public static MorphInfo[][][] analyzeText(String text) throws Exception {
     	if(HebrewNLP.getPassword() == null) {
     		throw new IllegalStateException("Please set HebrewNLP.setPassword() method with your password before using this method. To get a password register at https://hebrew-nlp.co.il/registration.");
     	}
-    	MorphRequest request = new MorphRequest();
+    	MorphologyRequest request = new MorphologyRequest();
     	request.token = HebrewNLP.getPassword();
     	request.text = text;
     	String requestJson = GSON.toJson(request);
@@ -131,7 +195,7 @@ public class HebrewMorphology {
     	if(HebrewNLP.getPassword() == null) {
     		throw new IllegalStateException("Please set HebrewNLP.setPassword() method with your password before using this method. To get a password register at https://hebrew-nlp.co.il/registration.");
     	}
-    	MorphRequest request = new MorphRequest();
+    	MorphologyRequest request = new MorphologyRequest();
     	request.token = HebrewNLP.getPassword();
     	request.sentences = sentences;
     	String requestJson = GSON.toJson(request);
@@ -146,7 +210,7 @@ public class HebrewMorphology {
     	if(HebrewNLP.getPassword() == null) {
     		throw new IllegalStateException("Please set HebrewNLP.setPassword() method with your password before using this method. To get a password register at https://hebrew-nlp.co.il/registration.");
     	}
-    	MorphRequest request = new MorphRequest();
+    	MorphologyRequest request = new MorphologyRequest();
     	request.token = HebrewNLP.getPassword();
     	request.sentence = sentence;
     	String requestJson = GSON.toJson(request);
@@ -165,7 +229,7 @@ public class HebrewMorphology {
     	if(HebrewNLP.getPassword() == null) {
     		throw new IllegalStateException("Please set HebrewNLP.setPassword() method with your password before using this method. To get a password register at https://hebrew-nlp.co.il/registration.");
     	}
-    	MorphRequest request = new MorphRequest();
+    	MorphologyRequest request = new MorphologyRequest();
     	request.token = HebrewNLP.getPassword();
     	request.words = words;
     	String requestJson = GSON.toJson(request);
--- Normalizer Example/src/il/co/hebrewnlp/example/NormalizeExample.java
+++ Normalizer Example/src/il/co/hebrewnlp/example/NormalizeExample.java
@@ -2,13 +2,14 @@ package il.co.hebrewnlp.example;
 
 import il.co.hebrewnlp.HebrewNLP;
 import il.co.hebrewnlp.morphology.HebrewMorphology;
+import il.co.hebrewnlp.morphology.HebrewMorphology.NormalizationType;
 
 public class NormalizeExample {
 
 	public static void main(String[] args) throws Exception {
 		HebrewNLP.setPassword("...");
 		
-		String[] options = HebrewMorphology.normalizeSentence("הילד שהלך לפרק ישב");
+		String[] options = HebrewMorphology.normalizeSentence("הילד שהלך לפרק ישב", NormalizationType.INDEX);
 
 		for(String option : options) {
 			System.out.println(option);
